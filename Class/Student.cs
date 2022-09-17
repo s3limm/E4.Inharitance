7f9b9fe2881ac80f2f030d349ec9127f41997f71
s3limm <yavuzselimemrem906@gmail.com>
2022-09-17 19:22:32 +0300

    added comment line

--- Class/Student.cs
+++ Class/Student.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace E4.İnharitance.Class
 {
-    internal class Student
+    internal class Student : User // student class'ının içerise user class'ının özelliklerini aktardık ve böylece kod kalabalığını önleyerek birden fazla class'ın içerisinde aynı özellikleri kullanabildik.
     {
+        public string studentNumber { get; set; }
+        public string classRoom { get; set; }
+
     }
 }
